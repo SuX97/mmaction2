eb285bdde69947bf3c88f926dfde8bed406ae0b9
CarolinaCheng616 <948005247@qq.com>
2021-01-13 16:05:25 +0800

    fixed_gt_len

--- train_bsn_pipeline.py
+++ train_bsn_pipeline.py
@@ -35,6 +35,8 @@ def train_bsn():
     gpus = 3
     epoch = 70
     dataset = 'trunet'
+    port = 29510
+
     tem_cmd = f"CUDA_VISIBLE_DEVICES={','.join(CUDA)} PORT=29501 " \
               "bash tools/dist_train.sh " \
               f"configs/localization/bsn/bsn_tem_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature.py {len(CUDA)}"
@@ -43,7 +45,7 @@ def train_bsn():
     os.system(f'mv work_dirs/bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/latest.pth '
               f'work_dirs/bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/tem_latest.pth')
 
-    tem_generate = f"CUDA_VISIBLE_DEVICES={CUDA[0]} PORT=29502 " \
+    tem_generate = f"CUDA_VISIBLE_DEVICES={CUDA[0]} PORT={port} " \
                    "bash tools/dist_test.sh " \
                    f"configs/localization/bsn/bsn_tem_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature.py " \
                    f"work_dirs/bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/tem_latest.pth 1"
@@ -54,10 +56,13 @@ def train_bsn():
               "--mode train"
     os.system(pgm_cmd)
 
-    pem_cmd = f"CUDA_VISIBLE_DEVICES={','.join(CUDA)} PORT=29503 " \
+    pem_cmd = f"CUDA_VISIBLE_DEVICES={','.join(CUDA)} PORT={port} " \
               "bash tools/dist_train.sh " \
               f"configs/localization/bsn/bsn_pem_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature.py {len(CUDA)}"
     os.system(pem_cmd)
+
+    os.system(f'mv work_dirs/bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/latest.pth '
+              f'work_dirs/bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/pem_latest.pth')
 
 
 if __name__ == '__main__':
