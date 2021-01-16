2e7d82aedf0ede2239a29de31624bada73841685
CarolinaCheng616 <948005247@qq.com>
2021-01-16 13:55:39 +0800

    original_dim

--- bsn_slurm_pipeline.py
+++ bsn_slurm_pipeline.py
@@ -10,44 +10,55 @@ def train_bsn():
               'configs/localization/bsn/' \
               f'bsn_tem_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature.py'\
               f' --cfg-options dist_params.port={port}'
-    os.system(tem_cmd)
 
-    os.system('mv work_dirs/'
-              f'bsn_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature/'
-              'latest.pth '
-              'work_dirs/'
-              f'bsn_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature/'
-              'tem_latest.pth')
+    mv_tem = 'mv work_dirs/' \
+             f'bsn_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature/' \
+             'latest.pth ' \
+             'work_dirs/' \
+             f'bsn_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature/' \
+             'tem_latest.pth'
 
     tem_gen = f'GPUS={GPUS} GPUS_PER_NODE={GPUS_PER_NODE} ' \
               f'bash tools/slurm_test.sh {part} {job_name} ' \
               'configs/localization/bsn/' \
               f'bsn_tem_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature.py'\
-              f' work_dirs/ ' \
+              f' work_dirs/' \
               f'bsn_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature/' \
               f'tem_latest.pth ' \
               f'--cfg-options dist_params.port={port}'
-    os.system(tem_gen)
 
     pgm_cmd = 'python tools/bsn_proposal_generation.py ' \
               'configs/localization/bsn/' \
               f'bsn_pgm_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature.py' \
               ' --mode train'
-    os.system(pgm_cmd)
 
     pem_cmd = f'GPUS={GPUS} GPUS_PER_NODE={GPUS_PER_NODE} ' \
               f'bash tools/slurm_train.sh {part} {job_name} ' \
               'configs/localization/bsn/' \
               f'bsn_pem_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature.py' \
               f' --cfg-options dist_params.port={port}'
-    os.system(pem_cmd)
 
-    os.system('mv work_dirs/'
-              f'bsn_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature/'
-              'latest.pth '
-              'work_dirs/'
-              f'bsn_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature/'
-              'pem_latest.pth')
+    mv_pem = 'mv work_dirs/' \
+             f'bsn_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature/' \
+             'latest.pth ' \
+             'work_dirs/' \
+             f'bsn_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature/' \
+             'pem_latest.pth'
+
+    flag = False
+    if tem_train or flag:
+        flag = True
+        os.system(tem_cmd)
+        os.system(mv_tem)
+    if tem or flag:
+        flag = True
+        os.system(tem_gen)
+    if pgm or flag:
+        flag = True
+        os.system(pgm_cmd)
+    if pem or flag:
+        os.system(pem_cmd)
+        os.system(mv_pem)
 
 
 def evaluate_bsn():
@@ -57,27 +68,24 @@ def evaluate_bsn():
               f'bash tools/slurm_test.sh {part} {job_name} ' \
               'configs/localization/bsn/' \
               f'bsn_tem_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature.py' \
-              f' work_dirs/ ' \
+              f' work_dirs/' \
               f'bsn_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature/' \
               f'tem_latest.pth ' \
               f'--cfg-options dist_params.port={port}'
-    os.system(tem_gen)
 
     pgm_cmd = 'python tools/bsn_proposal_generation.py ' \
               'configs/localization/bsn/' \
               f'bsn_pgm_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature.py'\
               ' --mode test'
-    os.system(pgm_cmd)
 
     pem_gen = f'GPUS={GPUS} GPUS_PER_NODE={GPUS_PER_NODE} ' \
               f'bash tools/slurm_test.sh {part} {job_name} ' \
               'configs/localization/bsn/' \
               f'bsn_pem_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature.py' \
-              f' work_dirs/ ' \
+              f' work_dirs/' \
               f'bsn_{shape}_{batch}x{GPUS}_{epoch}e_{dataset}_feature/' \
               f'pem_latest.pth ' \
               f'--cfg-options dist_params.port={port}'
-    os.system(pem_gen)
 
     eval_cmd = 'python tools/analysis/report_map.py ' \
         if dataset == 'activitynet' \
@@ -92,7 +100,19 @@ def evaluate_bsn():
                     'det_results.json '
     else:
         eval_cmd += '--gt data/TruNet/val_meta.json '
-    os.system(eval_cmd)
+
+    flag = False
+    if tem or flag:
+        flag = True
+        os.system(tem_gen)
+    if pgm or flag:
+        flag = True
+        os.system(pgm_cmd)
+    if pem or flag:
+        flag = True
+        os.system(pem_gen)
+    if mAP or flag:
+        os.system(eval_cmd)
 
 
 if __name__ == '__main__':
@@ -107,12 +127,20 @@ if __name__ == '__main__':
     parser.add_argument('--epoch', type=int)
     parser.add_argument('--dataset', type=str)
     parser.add_argument('--train', action='store_true')
+
+    parser.add_argument('--tem-train', action='store_true')
+    parser.add_argument('--tem-gen', action='store_true')
+    parser.add_argument('--pgm', action='store_true')
+    parser.add_argument('--pem', action='store_true')
+    parser.add_argument('--map', action='store_true')
     args = parser.parse_args()
     GPUS, GPUS_PER_NODE, part, job_name, port = (args.GPUS, args.GPUS_PER_NODE,
                                                  args.part, args.job_name,
                                                  args.port)
     shape, batch, epoch, dataset, train = (args.shape, args.batch, args.epoch,
                                            args.dataset, args.train)
+    tem_train, tem, pgm, pem, mAP = (args.tem_train, args.tem_gen, args.pgm,
+                                     args.pem, args.map)
     if train:
         train_bsn()
     else:
