fe062964d6f75845df5047556e34ec7b7fb9e09e
CarolinaCheng616 <948005247@qq.com>
2021-01-17 21:31:12 +0800

    original_dim

--- bsn_dist_pipeline.py
+++ bsn_dist_pipeline.py
@@ -10,14 +10,13 @@ def train_bsn():
               'configs/localization/bsn/' \
               f'bsn_tem_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature.py'\
               f' {len(CUDA)}'
-    os.system(tem_cmd)
 
-    os.system('mv work_dirs/'
-              f'bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/'
-              'latest.pth '
-              'work_dirs/'
-              f'bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/'
-              'tem_latest.pth')
+    mv_tem = 'mv work_dirs/' \
+             f'bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/' \
+             'latest.pth ' \
+             'work_dirs/' \
+             f'bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/' \
+             'tem_latest.pth'
 
     tem_gen = f'CUDA_VISIBLE_DEVICES={CUDA[0]} PORT={port} ' \
               'bash tools/dist_test.sh ' \
@@ -26,27 +25,47 @@ def train_bsn():
               ' work_dirs/' \
               f'bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/' \
               'tem_latest.pth 1'
-    os.system(tem_gen)
 
     pgm_cmd = 'python tools/bsn_proposal_generation.py ' \
               'configs/localization/bsn/' \
               f'bsn_pgm_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature.py'\
               ' --mode train'
-    os.system(pgm_cmd)
 
     pem_cmd = f"CUDA_VISIBLE_DEVICES={','.join(CUDA)} PORT={port} " \
               'bash tools/dist_train.sh ' \
               'configs/localization/bsn/' \
               f'bsn_pem_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature.py'\
               f' {len(CUDA)}'
-    os.system(pem_cmd)
 
-    os.system('mv work_dirs/'
-              f'bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/'
-              'latest.pth '
-              'work_dirs/'
-              f'bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/'
-              'pem_latest.pth')
+    mv_pem = 'mv work_dirs/' \
+             f'bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/' \
+             'latest.pth ' \
+             'work_dirs/' \
+             f'bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/' \
+             'pem_latest.pth'
+
+    flag = False
+    if tem_train or flag:
+        flag = True
+        status = os.system(tem_cmd)
+        if status != 0:
+            return
+        os.system(mv_tem)
+    if tem or flag:
+        flag = True
+        status = os.system(tem_gen)
+        if status != 0:
+            return
+    if pgm or flag:
+        flag = True
+        status = os.system(pgm_cmd)
+        if status != 0:
+            return
+    if pem or flag:
+        status = os.system(pem_cmd)
+        if status != 0:
+            return
+        os.system(mv_pem)
 
 
 def evaluate_bsn():
@@ -59,13 +78,11 @@ def evaluate_bsn():
               'work_dirs/' \
               f'bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/' \
               'tem_latest.pth 1'
-    os.system(tem_gen)
 
     pgm_cmd = 'python tools/bsn_proposal_generation.py ' \
               'configs/localization/bsn/' \
               f'bsn_pgm_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature.py'\
               ' --mode test'
-    os.system(pgm_cmd)
 
     pem_gen = f'CUDA_VISIBLE_DEVICES={CUDA[0]} PORT={port} ' \
               'bash tools/dist_test.sh ' \
@@ -74,7 +91,6 @@ def evaluate_bsn():
               ' work_dirs/' \
               f'bsn_{shape}_{batch}x{gpus}_{epoch}e_{dataset}_feature/' \
               'pem_latest.pth 1'
-    os.system(pem_gen)
 
     eval_cmd = 'python tools/analysis/report_map.py ' \
                if dataset == 'activitynet' \
@@ -89,7 +105,25 @@ def evaluate_bsn():
                     'det_results.json '
     else:
         eval_cmd += '--gt data/TruNet/val_meta.json '
-    os.system(eval_cmd)
+
+    flag = False
+    if tem or flag:
+        flag = True
+        status = os.system(tem_gen)
+        if status != 0:
+            return
+    if pgm or flag:
+        flag = True
+        status = os.system(pgm_cmd)
+        if status != 0:
+            return
+    if pem or flag:
+        flag = True
+        status = os.system(pem_gen)
+        if status != 0:
+            return
+    if mAP or flag:
+        os.system(eval_cmd)
 
 
 if __name__ == '__main__':
@@ -101,10 +135,18 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', type=str)
     parser.add_argument('--port', type=int)
     parser.add_argument('--train', action='store_true')
+    parser.add_argument('--tem-train', action='store_true')
+    parser.add_argument('--tem', action='store_true')
+    parser.add_argument('--pgm', action='store_true')
+    parser.add_argument('--pem', action='store_true')
+    parser.add_argument('--map', action='store_true')
+
     args = parser.parse_args()
     CUDA, shape, batch, epoch, dataset, port = (args.cuda, args.shape,
                                                 args.batch, args.epoch,
                                                 args.dataset, args.port)
+    tem_train, tem, pgm, pem, mAP = (args.tem_train, args.tem, args.pgm,
+                                     args.pem, args.map)
     gpus = len(CUDA)
     train = args.train
     if train:
